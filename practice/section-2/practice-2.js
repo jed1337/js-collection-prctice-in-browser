0eebf8ab51e3669b2d4dac09aeed767d1aa4765f
Jedidiah Ysamm Caychingco <jedidiah.ysamm.caychingco@oocl.com>
2019-09-12 16:44:36 +0800

    Finish section 2-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,38 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let sameElementArr = [];
+  for (let rawElement of collection) {
+    let parsedElement = parse(rawElement);
+
+    let letterDictionary = getLetterDictionary(parsedElement, sameElementArr);
+    if (letterDictionary != null) {
+      letterDictionary.count+=parsedElement.count;
+    } else {
+      sameElementArr.push(parsedElement);
+    }
+  }
+  return sameElementArr;
+}
+
+function parse(rawElement) {
+  let key = undefined;
+  let count = undefined;
+
+  if(rawElement.includes("-")){
+    [key, count] = rawElement.split("-");
+  } else{
+    [key, count] = [rawElement, 1];
+  }
+
+  return {key: key, count: parseInt(count)};
+}
+
+function getLetterDictionary(parsedElement, sameElementArr) {
+  for(let letterDictionary of sameElementArr){
+    if(letterDictionary.key === parsedElement.key){
+      return letterDictionary;
+    }
+  }
+  return null;
 }
