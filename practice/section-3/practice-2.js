339e5439a6178e796670bf3387f7b0ccc6cb1b06
Jedidiah Ysamm Caychingco <jedidiah.ysamm.caychingco@oocl.com>
2019-09-12 17:15:02 +0800

    Finish section 3-2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let updatedCollection = Array.from(collectionA);
+
+  let subtractValue = 1;
+  for(let dictionary of collectionA){
+    if(objectB.value.includes(dictionary.key)){
+      dictionary.count-=subtractValue;
+      subtractValue = updateSubtractValue(subtractValue);
+    }
+  }
+  return updatedCollection;
+}
+
+function updateSubtractValue(subtractValue) {
+  subtractValue++;
+  if (subtractValue % 4 === 0) {
+    subtractValue = 1;
+  }
+  return subtractValue;
 }
