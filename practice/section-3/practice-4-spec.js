1ca379618aa9e924dfb6132e0614f7dbdf0faf51
Jedidiah Ysamm Caychingco <jedidiah.ysamm.caychingco@oocl.com>
2019-09-13 08:46:08 +0800

    Finish section 3-4

--- practice/section-3/practice-4-spec.js
+++ practice/section-3/practice-4-spec.js
@@ -4,21 +4,25 @@
 describe('practice-3-4', () => {
 
   const collectionA = [
-    'a', 'a', 'a',
-    'e', 'e', 'e', 'e', 'e', 'e', 'e',
-    'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
-    't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
-    'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
-    'g', 'g', 'g', 'g', 'g', 'g', 'g',
-    'b', 'b', 'b', 'b', 'b', 'b',
-    'd-5'
+    /*3*/	'a', 'a', 'a',
+    /*7*/	'e', 'e', 'e', 'e', 'e', 'e', 'e',
+    /*11*/	'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
+    /*20*/	't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
+    /*9*/	'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    /*8*/	'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
+    /*7*/	'g', 'g', 'g', 'g', 'g', 'g', 'g',
+    /*6*/	'b', 'b', 'b', 'b', 'b', 'b',
+    /*5*/	'd-5'
   ];
 
 
   const objectB = {value: ['a', 'd', 'e', 'f']};
 
-  it('Counting the same elements in Collection A and get a new Collection C, if element includes "-", plus the right side number to count of this element. the elements in C should like {key:"a", count: 3}. then Get the same elements both in key properties of Collection C and value property of Collection B, counting them and following the rule which is full three minus one in Collection C, ouput the new Collection C', () => {
+  it('Counting the same elements in Collection A and get a new Collection C, if element includes "-", ' +
+    'plus the right side number to count of this element. the elements in C should like {key:"a", count: 3}. ' +
+    'then Get the same elements both in key properties of Collection C and value property of Collection B, ' +
+    'counting them and following the rule which is full three minus one in Collection C, ' +
+    'ouput the new Collection C', () => {
 
     const result = createUpdatedCollection(collectionA, objectB);
 
--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,63 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let collectionC = countSameElements(collectionA);
+
+  let subtractValue = 1;
+  for(let dictionary of collectionC){
+    if(objectB.value.includes(dictionary.key)){
+      dictionary.count-=subtractValue;
+      subtractValue = updateSubtractValue(subtractValue);
+    }
+  }
+  return collectionC;
+}
+
+function countSameElements(collection) {
+  let sameElementArr = [];
+  for (let rawElement of collection) {
+    let parsedElement = parse(rawElement);
+
+    let letterDictionary = getLetterDictionary(parsedElement.key, sameElementArr);
+    if (letterDictionary != null) {
+      letterDictionary.count+=parsedElement.count;
+    } else {
+      sameElementArr.push(parsedElement);
+    }
+  }
+  return sameElementArr;
+}
+
+function parse(rawElement) {
+  let key = undefined;
+  let count = undefined;
+
+  if(rawElement.includes("-")){
+    [key, count] = rawElement.split("-");
+  } else if(rawElement.includes(":")){
+    [key, count] = rawElement.split(":");
+  } else if(rawElement.includes("[") && rawElement.includes("]")){
+    [key, count] = rawElement.split("[");
+  } else{
+    [key, count] = [rawElement, 1];
+  }
+
+  return {key: key, count: parseInt(count)};
+}
+
+function getLetterDictionary(letter, sameElementArr) {
+  for(let letterDictionary of sameElementArr){
+    if(letterDictionary.key === letter){
+      return letterDictionary;
+    }
+  }
+  return null;
+}
+
+function updateSubtractValue(subtractValue) {
+  subtractValue++;
+  if (subtractValue % 4 === 0) {
+    subtractValue = 1;
+  }
+  return subtractValue;
 }
